52f581e2c47a4728f0aaa7366e88deb76dd3483c
michael-zucchetta <michael.zucchetta@gmail.com>
2016-05-30 23:36:26 +0000

    added pixel printing

--- projects/image_analysis/src/main.rs
+++ projects/image_analysis/src/main.rs
@@ -2,10 +2,16 @@ extern crate image;
 
 use std::fs::File;
 
+use image::{
+    GenericImage
+};
+
 fn main() {
 	println!("trying to open an image");
 	let img = image::open("src/resources/tizian.jpg").expect("Image was not opened");
 	let filtered = img.fliph();
 	let mut out = File::create("out.png").unwrap();
 	let _ = filtered.save(&mut out, image::PNG).expect("Saving image failed");
+    println!("dimensions {:?}", img.dimensions());
+    println!("printing imgage pixel value {:?}", img.get_pixel(100, 100));
 }
