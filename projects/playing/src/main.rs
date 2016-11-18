a07e7332154e5b11f40a3edd5cb98cf2f8a88de8
Michael Zucchetta <mzucchetta@gilt.com>
2016-11-18 13:02:03 +0000

    added some simple webserver and trait playground

---
+++ projects/playing/src/main.rs
@@ -0,0 +1,31 @@
+extern crate iron;
+
+use iron::prelude::*;
+use iron::status;
+
+fn main() {
+    trait Foo {
+        fn method(&self) -> String;
+    }
+    impl Foo for String {
+        fn method(&self) -> String { format!("string: {}", *self) }
+    }
+    
+    fn do_something(x: &Foo) {
+        x.method();
+    }
+    
+    fn hello_world(_: &mut Request) -> IronResult<Response> {
+        Ok(Response::with((status::Ok, "Hello World! ")))
+    }
+
+    let _server = Iron::new(hello_world).http("localhost:3000").unwrap();
+    let value = 3;
+    let ciao = || {
+        2 + value
+    };
+    let value_string = "something".to_string();
+    let x = do_something(&value_string);
+    // x prints as () as the value Foo does not implement the Display trait and :? allows it
+    println!("On 3000 {} {:?}", ciao(), x);
+}
